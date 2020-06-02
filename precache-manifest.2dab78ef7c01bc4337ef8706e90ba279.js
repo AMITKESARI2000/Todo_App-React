d4c0d4a871a7ac1d042a4d67c151a821ae924c53
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-06-03 01:55:08 +0530

    Updates

---
+++ precache-manifest.2dab78ef7c01bc4337ef8706e90ba279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f830e4c8e2128da09c585bc23f3b51",
+    "url": "/Todo_App-React/index.html"
+  },
+  {
+    "revision": "8b5768db2f00623da868",
+    "url": "/Todo_App-React/static/css/main.fb9eb860.chunk.css"
+  },
+  {
+    "revision": "c2858f020129e22c86c7",
+    "url": "/Todo_App-React/static/js/2.e4d6b287.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo_App-React/static/js/2.e4d6b287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b5768db2f00623da868",
+    "url": "/Todo_App-React/static/js/main.6294728b.chunk.js"
+  },
+  {
+    "revision": "d8afcf591ab127382ccc",
+    "url": "/Todo_App-React/static/js/runtime-main.8c26adcb.js"
+  },
+  {
+    "revision": "ea80e8bcf70c0a180cdc6905afb3f1c1",
+    "url": "/Todo_App-React/static/media/web-designer-pune-background.ea80e8bc.jpg"
+  }
+]);
